e180d9a7cf4913c95b88a653be6caed2004760f8
NadaKaram <93673728+NadaKaram@users.noreply.github.com>
2023-03-22 22:58:55 +0200

    adding the project

---
+++ c_project/Application/src/Application.h
@@ -0,0 +1,14 @@
+/*
+ * Application.h
+ *
+ *  Created on: Dec 17, 2022
+ *      Author: lenovo
+ */
+
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+#include "Server.h"
+void appStart(void);
+
+
+#endif /* APPLICATION_H_ */
